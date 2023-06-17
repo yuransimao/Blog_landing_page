3df8da1db7638702c1342e1dcf908f7f679187e9
Yuran Simao <yuransimao7@gmail.com>
2023-06-17 21:14:23 +0100

    estelizando navbar

--- src/Container/Navbar/index.jsx
+++ src/Container/Navbar/index.jsx
@@ -1,3 +1,36 @@
+import Styles from "./styles.module.scss"
+import {BiSearch} from "react-icons/bi"
+import {AiFillFacebook,AiOutlineTwitter,AiFillYoutube,AiFillInstagram,AiOutlineMenu} from "react-icons/ai"
+
 export function Navbar(){
-    return <h1>Ola </h1>
+    return (
+        <div className={Styles['Navbar']}>
+            <div className={Styles["Logo"]}><a href="#">Logo</a></div>
+            <div className={Styles["Container-nav"]}>
+                <div className={Styles['Nav-link']}>
+                    
+                    <ul>
+                        <li><a href="#">Categories</a></li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+                </div>
+                <div className={Styles['Container']}>
+                    <div className={Styles["Search"]}>
+                        <input type="search" placeholder ="Search"/>
+                        <span><BiSearch/></span>
+                    </div>
+                    <div className={Styles['Icons']}>
+                        <ul>
+                        <li>Facebook</li>
+                        <li>Instagram</li>
+                        <li>Twitter</li>
+                        <li>Youtube</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div className={Styles["Menu"]}> <button><AiOutlineMenu/></button></div>
+        </div>
+    )
 }

168d9e75072b98214226d966cadb3c3e7c786b6e
Yuran Simao <yuransimao7@gmail.com>
2023-06-17 21:43:33 +0100

    update

--- src/Container/Navbar/index.jsx
+++ src/Container/Navbar/index.jsx
@@ -1,12 +1,24 @@
 import Styles from "./styles.module.scss"
 import {BiSearch} from "react-icons/bi"
+import {RxCross2} from "react-icons/rx"
 import {AiFillFacebook,AiOutlineTwitter,AiFillYoutube,AiFillInstagram,AiOutlineMenu} from "react-icons/ai"
+import { useState } from "react"
 
 export function Navbar(){
+
+    const [show, setShow] = useState(false)
+
+    window.addEventListener('resize', () =>{
+        if(window.innerWidth > 768){
+            setShow(false)
+        }
+    })
     return (
         <div className={Styles['Navbar']}>
             <div className={Styles["Logo"]}><a href="#">Logo</a></div>
-            <div className={Styles["Container-nav"]}>
+
+
+            <div className={show ? Styles["Responsive-nav"]:Styles["Container-nav"]}>
                 <div className={Styles['Nav-link']}>
                     
                     <ul>
@@ -22,15 +34,17 @@ export function Navbar(){
                     </div>
                     <div className={Styles['Icons']}>
                         <ul>
-                        <li>Facebook</li>
-                        <li>Instagram</li>
-                        <li>Twitter</li>
-                        <li>Youtube</li>
+                        <li><AiFillFacebook/></li>
+                        <li><AiFillInstagram/></li>
+                        <li><AiOutlineTwitter/></li>
+                        <li><AiFillYoutube/></li>
                         </ul>
                     </div>
                 </div>
             </div>
-            <div className={Styles["Menu"]}> <button><AiOutlineMenu/></button></div>
+            <div className={Styles["Menu"]} > <button onClick={() => setShow(!show)}>
+                {show ? <RxCross2/> : <AiOutlineMenu/>}
+                </button></div>
         </div>
     )
 }
